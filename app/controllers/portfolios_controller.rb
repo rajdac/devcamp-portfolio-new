cc5b1df9f048d5ef1ded7981eaddf5a286cdc27b
Rajeev Singh <rajdac@gmail.com>
2021-06-14 23:44:03 +0530

    Added destroy feature for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -47,4 +47,16 @@ class PortfoliosController < ApplicationController
 
   end	
 
+  def destroy
+  	# perform lookup
+  	@portfolio_item = Portfolio.find(params[:id])
+    
+    # Destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio item was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end  	
+
 end
