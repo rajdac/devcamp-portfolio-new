af23a265f713935583ff4399c5dff1baa3db9e17
Rajeev Singh <rajdac@gmail.com>
2021-06-17 00:24:31 +0530

    added guest user

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,13 @@
+module CurrentUserConcern
+    extend ActiveSupport::Concern
+    def current_user
+    	super || guest_user
+    end 
+
+    def guest_user
+        OpenStruct.new(name: "Guest user",
+                       first_name: "Guest",
+                       last_name: "user",
+                       email: "asdf@asdf" )
+    end    
+end    
